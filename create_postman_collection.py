0fe2f1327f43ff4f846ec0d89214252d21e9ece2
Mariana <mifologic@gmail.com>
2024-03-27 00:40:33 +0300

    Update API link and collection creation

--- create_postman_collection.py
+++ create_postman_collection.py
@@ -1,60 +1,39 @@
 import json
 
-
-clothes_size = ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']  # данные, которые будут использованы в запросах
+product_categories = ["electronics", "jewelery", "men's clothing", "women's clothing"]  # данные, которые будут использованы в запросах
 
 items = []  # список, в который будут сохранены сгенерированные запросы
-for size in clothes_size:
+for category in product_categories:
     item = {  # item – описание запроса Postman
-        "name": f"Size {size}",
-        "protocolProfileBehavior": {
-            "disableBodyPruning": True
-        },
+        "name": f"Category {category}",
+        "event": [
+            {
+                "listen": "test",
+                "script": {
+                    "exec": [
+                        "pm.test(\"Status code is 200\", function () {",
+                        "    pm.response.to.have.status(200);",
+                        "});"
+                    ],
+                    "type": "text/javascript",
+                    "packages": {}
+                }
+            }
+        ],
         "request": {
             "method": "GET",
             "header": [],
-            "body": {
-                "mode": "raw",
-                "raw": "",
-                "options": {
-                    "raw": {
-                        "language": "json"
-                    }
-                }
-            },
             "url": {
-                "raw": f"https://ir-example.mir.prod.reco.microsoft.com/Reco/V1.0/Popular?modeling=adw&Count=5&Category=Clothing&Size={size}",
+                "raw": f"https://fakestoreapi.com/products/category/{category}",
                 "protocol": "https",
                 "host": [
-                    "ir-example",
-                    "mir",
-                    "prod",
-                    "reco",
-                    "microsoft",
+                    "fakestoreapi",
                     "com"
                 ],
                 "path": [
-                    "Reco",
-                    "V1.0",
-                    "Popular"
-                ],
-                "query": [
-                    {
-                        "key": "modeling",
-                        "value": "adw"
-                    },
-                    {
-                        "key": "Count",
-                        "value": "5"
-                    },
-                    {
-                        "key": "Category",
-                        "value": "Clothing"
-                    },
-                    {
-                        "key": "Size",
-                        "value": f"{size}"
-                    }
+                    "products",
+                    "category",
+                    f"{category}"
                 ]
             }
         },
@@ -65,12 +44,12 @@ for size in clothes_size:
 postman_collection = {
     "info": {
         "_postman_id": "f1ad7686-7f54-44ee-a20b-534bb1f028e0",
-        "name": "Test Example",
+        "name": "Generated Collection",
         "schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json",
         "_exporter_id": "15037634"
     },
     "item": items
 }
 
-with open('postman_collection.json', 'w') as f:
+with open('generated_collection.postman_collection.json', 'w') as f:
     json.dump(postman_collection, f)
